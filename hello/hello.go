b0b7ce432354f3f1b9e0b7302ddae86a39da949a
thepudds <thepudds@example.com>
2018-12-19 00:17:55 +0000

    Update starterlib with new backwards compatible functionality

---
+++ hello/hello.go
@@ -0,0 +1,7 @@
+package hello
+
+import "fmt"
+
+func Hello(who string) {
+        fmt.Printf("Hello, %s!\n", who)
+}
--- wise.go
+++ wise.go
@@ -1,9 +1,10 @@
-
 package starterlib
+
+import "github.com/thepudds-example-staging/starterlib/hello"
 
 import "fmt"
 
 func WiseHello() {
-        fmt.Println("Hello, World!")
+        hello.Hello("World")
         fmt.Println("Don't communicate by sharing memory, share memory by communicating.")
 }
