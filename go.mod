d3e55d103bb89433b14cca971578bc7fe838d4d7
thepudds <thepudds@example.com>
2018-12-19 00:17:48 +0000

    Initial commit

---
+++ go.mod
@@ -0,0 +1,1 @@
+module github.com/thepudds-example-staging/starterlib
---
+++ wise.go
@@ -0,0 +1,9 @@
+
+package starterlib
+
+import "fmt"
+
+func WiseHello() {
+        fmt.Println("Hello, World!")
+        fmt.Println("Don't communicate by sharing memory, share memory by communicating.")
+}
